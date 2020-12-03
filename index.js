b363d5498c9fe8ec5e5b82bcb95f586246efcb08
Lucas Rocha <67298525+lucasrocharodrigues@users.noreply.github.com>
2020-12-03 15:01:16 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const parentList = [
+  {
+    name: "Marcos Maia",
+    children: "Clarice",
+  },
+  {
+    name: "João Paulo",
+    children: "Pedro",
+  },
+  {
+    name: "Fernanda",
+    children: "Rosa",
+  },
+  {
+    name: "Patricia",
+    children: "Ana",
+  },
+  {
+    name: "Lucas",
+    children: "Samira",
+  },
+
+]
+
+function getParent(list, children){
+  return
+}
+
+const parent = getParent(parentList, 'Pedro')
+
+console.log(parent)
+
+//a função deve retornar o nome do pai de acordo com o nome de seu filho passado no segundo parâmetro 
